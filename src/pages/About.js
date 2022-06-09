3e6ee90fd02e730431c4f779d5167ca10193156b
Shreeya Patel <shreeyapaatel@gmail.com>
2022-06-09 16:38:37 -0600

    Initial Commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import './About.css'
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div class="split left">
+          <div className="centered">
+            <img 
+            className="profile_image"
+            src = {require('../images/shreeyaimage.png').default}
+            alt="Profile Pic"
+            ></img>
+        </div>
       </div>
+      <div className="split right">
+        <div className="centered">
+          <div className="name_title">Shreeya Patel</div>
+          <div className="brief_description">
+          Hello, I am a rising sophomore studying Computer Science at Columbia University. I am from Northern Canada and my interests include hiking, reading, and watching Star Wars.
+          </div>
+        </div>
+      </div>
+    </div>
     )
   }
 }
